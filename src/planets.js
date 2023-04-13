282da1b060ecaabec1dea2ce9fd83d915420144d
Matthew Butcher <99278651+mbutcherdev@users.noreply.github.com>
2023-04-13 15:58:57 +0100

    Planets: Large upgrade - Changed the design of the page - Carousel is working - Pull the planets data from JSON file with JS - Jumping was caused by "vh" in CSS, removed.

---
+++ src/planets.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("src/data/planet-data.json")
+    .then((response) => response.json())
+    .then((planets) => {
+      const carouselContent = document.querySelector(".carousel-content");
+
+      planets.forEach((planet) => {
+        const planetSlide = document.createElement("div");
+        planetSlide.classList.add("planet-slide");
+
+        const planetImg = document.createElement("div");
+        planetImg.classList.add("planet-img");
+        const img = document.createElement("img");
+        img.src = planet.image;
+        img.alt = `Image of ${planet.name}`;
+        planetImg.appendChild(img);
+
+        const planetInfo = document.createElement("div");
+        planetInfo.classList.add("planet-info");
+
+        const name = document.createElement("h2");
+        name.textContent = `Name: ${planet.name}`;
+        const orbitLocation = document.createElement("p");
+        orbitLocation.textContent = `Orbit location: ${planet.orbitLocation}`;
+        const orbitDuration = document.createElement("p");
+        orbitDuration.textContent = `Duration of orbit: ${planet.orbitDuration}`;
+        const moons = document.createElement("p");
+        moons.textContent = `Moons: ${planet.moons}`;
+
+        planetInfo.appendChild(name);
+        planetInfo.appendChild(orbitLocation);
+        planetInfo.appendChild(orbitDuration);
+        planetInfo.appendChild(moons);
+
+        planetSlide.appendChild(planetImg);
+        planetSlide.appendChild(planetInfo);
+        carouselContent.appendChild(planetSlide);
+      });
+
+      initCarousel();
+    })
+    .catch((error) => {
+      console.error("Error fetching planet data:", error);
+    });
+
+  function initCarousel() {
+    const slides = document.querySelectorAll(".planet-slide");
+    const prevBtn = document.querySelector(".carousel-btn.prev-btn");
+    const nextBtn = document.querySelector(".carousel-btn.next-btn");
+    let currentSlide = 0;
+
+    slides[currentSlide].classList.add("active");
+
+    prevBtn.addEventListener("click", function () {
+      goToSlide(currentSlide - 1);
+    });
+
+    nextBtn.addEventListener("click", function () {
+      goToSlide(currentSlide + 1);
+    });
+
+    function goToSlide(n) {
+      slides[currentSlide].classList.remove("active");
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].classList.add("active");
+    }
+  }
+});
